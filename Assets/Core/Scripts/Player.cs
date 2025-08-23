dc8fda8438ec7abe730d692a50e4db7ae4d47d21
Zoltan Vegh <zoli0829@gmail.com>
2025-08-23 10:29:51 +0100

    implemented basic player movement

--- Assets/Core/Scripts/Player.cs
+++ Assets/Core/Scripts/Player.cs
@@ -1,12 +1,19 @@
+using System.Collections;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public float speed;
+    
+    private LayerMask obstacleMask;
+    private Vector2 targetPos;
     Transform GFX;
     float flipX;
+    private bool isMoving;
     
     void Start()
     {
+        obstacleMask = LayerMask.GetMask("Wall", "Enemy");
         GFX = GetComponentInChildren<SpriteRenderer>().transform;
         flipX = GFX.localScale.x;
     }
@@ -14,9 +21,45 @@ public class Player : MonoBehaviour
     void Update()
     {
         float horz = System.Math.Sign(Input.GetAxisRaw("Horizontal"));
-        if (Mathf.Abs(horz) > 0) 
+        float vert = System.Math.Sign(Input.GetAxisRaw("Vertical"));
+        if (Mathf.Abs(horz) > 0 || Mathf.Abs(vert) > 0)
         {
-            GFX.localScale = new Vector2(flipX * horz, GFX.localScale.y);
+            // flipping the sprite
+            if (Mathf.Abs(horz) > 0) 
+            {
+                GFX.localScale = new Vector2(flipX * horz, GFX.localScale.y);
+            }
+
+            if (!isMoving)
+            {
+                if (Mathf.Abs(horz) > 0)
+                {
+                    targetPos = new Vector2(transform.position.x + horz, transform.position.y);
+                }
+                else if (Mathf.Abs(vert) > 0)
+                {
+                    targetPos = new Vector2(transform.position.x, transform.position.y + vert);
+                }
+                // check for collisions
+                Vector2 hitSize = Vector2.one * 0.8f;
+                Collider2D hit = Physics2D.OverlapBox(targetPos, hitSize, 0, obstacleMask);
+                if (!hit)
+                {
+                    StartCoroutine(SmoothMove());
+                }
+            }
         }
+    }
+
+    IEnumerator SmoothMove()
+    {
+        isMoving = true;
+        while (Vector2.Distance(transform.position, targetPos) > 0.01f)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, targetPos, speed * Time.deltaTime);
+            yield return null;
+        }
+        transform.position = targetPos;
+        isMoving = false;
     }
 }
